21d4f5b5ab1f5093f801e9b3387aa1cb7922a409
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2022-06-05 19:07:01 +0000

    Add javascript code for incremental scoring system.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,40 +5,57 @@ addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type")==="top-row-left-dial") {
                 incrementClicksTopLeft ();
                 rotateRingTopLeft ();
-                matchingValuesTopLeft ()
-                
+                matchingValuesTopLeft ();
+                score ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="middle-row-left-dial") {
                 incrementClicksMiddleLeft ();
                 rotateRingMiddleLeft ();
-                matchingValuesMiddleLeft ()
+                matchingValuesMiddleLeft ();
+                score4 ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="bottom-row-left-dial") {
                 incrementClicksBottomLeft ();
                 rotateRingBottomLeft ();
-                matchingValuesBottomLeft ()
+                matchingValuesBottomLeft ();
+                score7 ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="top-row-middle-dial") {
                 incrementClicksTopMiddle ();
                 rotateRingTopMiddle ();
-                matchingValuesTopMiddle ()
+                matchingValuesTopMiddle ();
+                score2 ();
+                totalScore ();
             }  else if (this.getAttribute("data-type")==="middle-row-center-dial") {
                 incrementClicksCenter ();
                 rotateRingCenter ();
-                matchingValuesCenter ()
+                matchingValuesCenter ();
+                score5 ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="bottom-row-center-dial") {
                 incrementClicksBottomMiddle ();
                 rotateRingBottomMiddle ();
-                matchingValuesBottomMiddle ()
+                matchingValuesBottomMiddle ();
+                score8 ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="top-row-right-dial") {
                 incrementClicksTopRight ();
                 rotateRingTopRight ();
-                matchingValuesTopRight ()
+                matchingValuesTopRight ();
+                score3 ();
+                totalScore ();
             }  else if (this.getAttribute("data-type")==="middle-row-right-dial") {
                 incrementClicksMiddleRight ();
                 rotateRingMiddleRight ();
-                matchingValuesMiddleRight ()
+                matchingValuesMiddleRight ();
+                score6 ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="bottom-row-right-dial") {
                 incrementClicksBottomRight ();
                 rotateRingBottomRight ();
-                matchingValuesBottomRight ()
+                matchingValuesBottomRight ();
+                score9 ();
+                totalScore ();
             } 
     })
     }
@@ -46,10 +63,7 @@ addEventListener("DOMContentLoaded", function() {
     document.getElementById("start-game").addEventListener("click", parentCountdown);
     document.getElementById("start-game").addEventListener("click", resetGame);
     document.getElementById("start-game").addEventListener("click", defaultGameStatusMessage);
-
     
-    
-
 })
 
 
@@ -62,38 +76,56 @@ if (this.getAttribute("data-type")==="top-row-left-inner-dial") {
                 incrementClicksTopLeftInnerDial ();
                 rotateRingTopLeftInnerDial ();
                 matchingValuesTopLeft ()
+                score ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="middle-row-left-inner-dial") {
                 incrementClicksMiddleLeftInnerDial ();
                 rotateRingMiddleLeftInnerDial ();
-                matchingValuesMiddleLeft ()
+                matchingValuesMiddleLeft ();
+                score4 ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="bottom-row-left-inner-dial") {
                 incrementClicksBottomLeftInnerDial ();
                 rotateRingBottomLeftInnerDial ();
-                matchingValuesBottomLeft ()
+                matchingValuesBottomLeft ();
+                score7 ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="top-row-middle-inner-dial") {
                 incrementClicksTopMiddleInnerDial ();
                 rotateRingTopMiddleInnerDial ();
-                matchingValuesTopMiddle ()
+                matchingValuesTopMiddle ();
+                score2 ();
+                totalScore ();
             }  else if (this.getAttribute("data-type")==="middle-row-center-inner-dial") {
                 incrementClicksCenterInnerDial ();
                 rotateRingCenterInnerDial ();
-                matchingValuesCenter ()
+                matchingValuesCenter ();
+                score5 ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="bottom-row-center-inner-dial") {
                 incrementClicksBottomMiddleInnerDial ();
                 rotateRingBottomMiddleInnerDial ();
-                matchingValuesBottomMiddle ()
+                matchingValuesBottomMiddle ();
+                score8 ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="top-row-right-inner-dial") {
                 incrementClicksTopRightInnerDial ();
                 rotateRingTopRightInnerDial ();
                 matchingValuesTopRight ()
+                score3 ();
+                totalScore ();
             }  else if (this.getAttribute("data-type")==="middle-row-right-inner-dial") {
                 incrementClicksMiddleRightInnerDial ();
                 rotateRingMiddleRightInnerDial ();
-                matchingValuesMiddleRight ()
+                matchingValuesMiddleRight ();
+                score6 ();
+                totalScore ();
             } else if (this.getAttribute("data-type")==="bottom-row-right-inner-dial") {
                 incrementClicksBottomRightInnerDial ();
                 rotateRingBottomRightInnerDial ();
-                matchingValuesBottomRight ()
+                matchingValuesBottomRight ();
+                score9 ();
+                totalScore ();
             }
 
     })
@@ -1324,10 +1356,284 @@ function test () {
 
 function winRound () {
     let winRound = document.getElementById("game-over");
+    // let score =  document.getElementById("score");
     winRound.innerText = "Launch sequence disabled";
+    // score.innerText = 1000;
 }
 
 function defaultGameStatusMessage () {
    let defaultMessage = document.getElementById("game-over");
    defaultMessage.innerText = " ";
+}
+
+
+// score 1
+function score () {
+    
+    let topLeftCenterDial = document.getElementById("top-left-center-dial"); 
+    let score = parseInt(document.getElementById("score-1").innerText);
+
+    
+    if (topLeftCenterDial.style.backgroundColor == "green" &&  score <= 0) {
+        incrementScore ();
+    } else if (topLeftCenterDial.style.backgroundColor == "red" || "Blue" || "purple") {
+        decreaseScore ()
+    }
+}
+
+function incrementScore () {
+    let oldScore = parseInt(document.getElementById("score-1").innerText);
+    document.getElementById("score-1").innerText += 1000;
+
+}
+
+function decreaseScore () {
+    let oldScore = parseInt(document.getElementById("score-1").innerText);
+    if (oldScore >= 1) {
+        document.getElementById("score-1").innerText -= 1000;
+    }
+}
+    
+// score 2
+function score2 () {
+    
+    let topMiddleCenterDial = document.getElementById("top-middle-center-dial");
+    let score = parseInt(document.getElementById("score-2").innerText);
+
+    if (topMiddleCenterDial.style.backgroundColor == "green" &&  score <= 0) {
+        incrementScore2 ();
+    } else if (topMiddleCenterDial.style.backgroundColor == "red" || "Blue" || "purple") {
+        decreaseScore2 ()
+    }
+}
+
+
+function incrementScore2 () {
+    let oldScore = parseInt(document.getElementById("score-2").innerText);
+    document.getElementById("score-2").innerText += 1000;
+
+}
+
+function decreaseScore2 () {
+    let oldScore = parseInt(document.getElementById("score-2").innerText);
+    if (oldScore >= 1) {
+        document.getElementById("score-2").innerText -= 1000;
+    }
+}
+
+// score 3
+function score3 () {
+    
+    let topRightCenterDial = document.getElementById("top-right-center-dial");
+    let score = parseInt(document.getElementById("score-3").innerText);
+
+    if (topRightCenterDial.style.backgroundColor == "green" &&  score <= 0) {
+        incrementScore3 ();
+    } else if (topRightCenterDial.style.backgroundColor == "red" || "Blue" || "purple") {
+        decreaseScore3 ()
+    }
+}
+
+function incrementScore3 () {
+    let oldScore = parseInt(document.getElementById("score-3").innerText);
+    document.getElementById("score-3").innerText += 1000;
+
+}
+
+function decreaseScore3 () {
+    let oldScore = parseInt(document.getElementById("score-3").innerText);
+    if (oldScore >= 1) {
+        document.getElementById("score-3").innerText -= 1000;
+    }
+}
+
+
+// score 4
+function score4 () {
+    
+    let middleLeftCenterDial = document.getElementById("middle-left-center-dial");
+    let score = parseInt(document.getElementById("score-4").innerText);
+
+    if (middleLeftCenterDial.style.backgroundColor == "green" &&  score <= 0) {
+        incrementScore4 ();
+    } else if (middleLeftCenterDial.style.backgroundColor == "red" || "Blue" || "purple") {
+        decreaseScore4 ()
+    }
+}
+
+function incrementScore4 () {
+    let oldScore = parseInt(document.getElementById("score-4").innerText);
+    document.getElementById("score-4").innerText += 1000;
+
+}
+
+function decreaseScore4 () {
+    let oldScore = parseInt(document.getElementById("score-4").innerText);
+    if (oldScore >= 1) {
+        document.getElementById("score-4").innerText -= 1000;
+    }
+}
+
+
+
+// score 5
+function score5 () {
+    
+    let CenterDial = document.getElementById("center-dial");
+    let score = parseInt(document.getElementById("score-5").innerText);
+
+    if (CenterDial.style.backgroundColor == "green" &&  score <= 0) {
+        incrementScore5 ();
+    } else if (CenterDial.style.backgroundColor == "red" || "Blue" || "purple") {
+        decreaseScore5 ();
+    }
+}
+
+function incrementScore5 () {
+    let oldScore = parseInt(document.getElementById("score-5").innerText);
+    document.getElementById("score-5").innerText += 1000;
+
+}
+
+function decreaseScore5 () {
+    let oldScore = parseInt(document.getElementById("score-5").innerText);
+    if (oldScore >= 1) {
+        document.getElementById("score-5").innerText -= 1000;
+    }
+}
+
+
+// score 6
+function score6 () {
+    
+    let middleRightCenterDial = document.getElementById("middle-right-center-dial");
+    let score = parseInt(document.getElementById("score-6").innerText);
+
+    if (middleRightCenterDial.style.backgroundColor == "green" &&  score <= 0) {
+        incrementScore6 ();
+    } else if (middleRightCenterDial.style.backgroundColor == "red" || "Blue" || "purple") {
+        decreaseScore6 ();
+    }
+}
+
+function incrementScore6 () {
+    let oldScore = parseInt(document.getElementById("score-6").innerText);
+    document.getElementById("score-6").innerText += 1000;
+
+}
+
+function decreaseScore6 () {
+    let oldScore = parseInt(document.getElementById("score-6").innerText);
+    if (oldScore >= 1) {
+        document.getElementById("score-6").innerText -= 1000;
+    }
+}
+
+
+// score 7
+function score7 () {
+    
+    let bottomLeftCenterDial = document.getElementById("bottom-left-center-dial");
+    let score = parseInt(document.getElementById("score-7").innerText);
+
+    if (bottomLeftCenterDial.style.backgroundColor == "green" &&  score <= 0) {
+        incrementScore7 ();
+    } else if (bottomLeftCenterDial.style.backgroundColor == "red" || "Blue" || "purple") {
+        decreaseScore7 ();
+    }
+}
+
+function incrementScore7 () {
+    let oldScore = parseInt(document.getElementById("score-7").innerText);
+    document.getElementById("score-7").innerText += 1000;
+
+}
+
+function decreaseScore7 () {
+    let oldScore = parseInt(document.getElementById("score-7").innerText);
+    if (oldScore >= 1) {
+        document.getElementById("score-7").innerText -= 1000;
+    }
+}
+
+// score 8
+function score8 () {
+    
+    let bottomMiddleCenterDial = document.getElementById("bottom-middle-center-dial");
+    let score = parseInt(document.getElementById("score-8").innerText);
+
+    if (bottomMiddleCenterDial.style.backgroundColor == "green" &&  score <= 0) {
+        incrementScore8 ();
+    } else if (bottomMiddleCenterDial.style.backgroundColor == "red" || "Blue" || "purple") {
+        decreaseScore8 ();
+    }
+}
+
+function incrementScore8 () {
+    let oldScore = parseInt(document.getElementById("score-8").innerText);
+    document.getElementById("score-8").innerText += 1000;
+
+}
+
+function decreaseScore8 () {
+    let oldScore = parseInt(document.getElementById("score-8").innerText);
+    if (oldScore >= 1) {
+        document.getElementById("score-8").innerText -= 1000;
+    }
+}
+
+// score 9
+function score9 () {
+    
+    let bottomRightCenterDial = document.getElementById("bottom-right-center-dial");
+    let score = parseInt(document.getElementById("score-9").innerText);
+
+    if (bottomRightCenterDial.style.backgroundColor == "green" &&  score <= 0) {
+        incrementScore9 ();
+    } else if (bottomRightCenterDial.style.backgroundColor == "red" || "Blue" || "purple") {
+        decreaseScore9 ();
+    }
+}
+
+function incrementScore9 () {
+    let oldScore = parseInt(document.getElementById("score-9").innerText);
+    document.getElementById("score-9").innerText += 1000;
+
+}
+
+function decreaseScore9 () {
+    let oldScore = parseInt(document.getElementById("score-9").innerText);
+    if (oldScore >= 1) {
+        document.getElementById("score-9").innerText -= 1000;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function totalScore () {
+    let score1 = parseInt(document.getElementById("score-1").innerText); 
+    let score2 = parseInt(document.getElementById("score-2").innerText); 
+    let score3 = parseInt(document.getElementById("score-3").innerText); 
+    let score4 = parseInt(document.getElementById("score-4").innerText);
+    let score5 = parseInt(document.getElementById("score-5").innerText); 
+    let score6 = parseInt(document.getElementById("score-6").innerText); 
+    let score7 = parseInt(document.getElementById("score-7").innerText); 
+    let score8 = parseInt(document.getElementById("score-8").innerText);
+    let score9 = parseInt(document.getElementById("score-9").innerText); 
+    document.getElementById("score").innerText = score1 + score2 + score3 + score4 + score5 + score6 + score7 + score8 + score9;
 }
